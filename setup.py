9eb9a83c7d30d7f9bdd72a17c91963cbda14dae3
Qwerty-Qwerty88 <113751596+Qwerty-Qwerty88@users.noreply.github.com>
2023-06-11 05:45:41 +0000

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3",
+    "Topic :: Games/Entertainment"
+]
+ 
+setup(
+    name="TextAdventures",
+    version="0.0.1",
+    description="A Python library for easily making text adventure games.",
+    long_description=open("README.txt").read() + "\n\n" + open("CHANGELOG.txt").read(),
+    url="https://github.com/Qwerty-Qwerty88/Text-Adventures",  
+    author="Qwerty Qwerty",
+    author_email="personqwertyperson88@gmail.com",
+    license="MIT", 
+    classifiers=classifiers
+)
